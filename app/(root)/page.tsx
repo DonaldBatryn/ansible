e486b6c7eebe79be170b5476c7ab76f6cdd2c1c2
Donald Batryn <donald.batryn@gmail.com>
2025-01-19 01:00:13 -0600

    loading, not found, product components, prisma init

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,17 @@
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
+
 const Homepage = () => {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      Homepage
+      <ProductList
+        data={sampleData.products}
+        title="Newest arrivals"
+        limit={25}
+      />
+    </div>
+  );
 };
 
 export default Homepage;
---
+++ components/shared/product/product-card.tsx
@@ -0,0 +1,48 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+  CardHeader,
+  CardFooter,
+} from "@/components/ui/card";
+import Link from "next/link";
+import Image from "next/image";
+import ProductPrice from "./product-price";
+
+const ProductCard = ({ product }: { product: any }) => {
+  console.log(product);
+
+  return (
+    <Card className="w-full max-w-sm m-3">
+      <CardHeader className="p-0 items-center">
+        <Link href={`/product/${product.slug}`}>
+          <Image
+            src={product.images[0]}
+            alt={product.name}
+            height={300}
+            width={300}
+            priority={true}
+          />
+        </Link>
+      </CardHeader>
+      <CardContent className="p-4 grid gap-4">
+        <div className="text-xs">{product.brand}</div>
+        <Link href={`/product/${product.slug}`}>
+          <h2 className="text-sm font-medium">{product.name}</h2>
+        </Link>
+        <div className="flex-between gap-4">
+          <p>{product.rating} Stars</p>
+          {product.stock > 0 ? (
+            <ProductPrice value={Number(product.price)} />
+          ) : (
+            <p className="text-destructive">Out of stock</p>
+          )}
+        </div>
+      </CardContent>
+      <CardFooter></CardFooter>
+    </Card>
+  );
+};
+
+export default ProductCard;
